5e25c98b05303acc6fb2a698a2d837072b2f83be
lipandr <andrey.lipayev@gmail.com>
2023-04-20 14:33:13 +0300

    Creating a Users Model

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -6,6 +6,8 @@ import (
 )
 
 var ErrNoRecord = errors.New("models: no matching record found")
+var ErrInvalidCredentials = errors.New("models: invalid credentials")
+var ErrDuplicateEmail = errors.New("models: duplicate email")
 
 type Snippet struct {
 	ID      int       `json:"id"`
@@ -13,4 +15,13 @@ type Snippet struct {
 	Content string    `json:"content"`
 	Created time.Time `json:"created"`
 	Expires time.Time `json:"expires"`
+}
+
+type User struct {
+	ID             int       `json:"id"`
+	Name           string    `json:"name"`
+	Email          string    `json:"email"`
+	HashedPassword []byte    `json:"-"`
+	Created        time.Time `json:"-"`
+	Activated      bool      `json:"-"`
 }
